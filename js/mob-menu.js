676e30049b3750b1d6229775a36bfaf1c44fddc8
Denis Strashnyuk <strashnyuk.denis@gmail.com>
2022-01-01 19:34:50 +0200

    правки моб меню

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,9 +1,17 @@
 const btnOpen = document.querySelector('.js-open-menu');
+const btnClose = document.querySelector('.js-close-menu');
 const menuRef = document.querySelector('.js-mob-nav');
-const anchorLink = document.querySelectorAll('.js-link-close');
+// const anchorLink = document.querySelectorAll('.js-link-close');
 
 btnOpen.addEventListener('click', () => {
   menuRef.classList.toggle('open');
   btnOpen.classList.toggle('open');
+  btnClose.classList.toggle('open');
+  document.body.classList.toggle('js-body-hidden');
+});
+btnClose.addEventListener('click', () => {
+  menuRef.classList.toggle('open');
+  btnOpen.classList.toggle('open');
+  btnClose.classList.toggle('open');
   document.body.classList.toggle('js-body-hidden');
 });
